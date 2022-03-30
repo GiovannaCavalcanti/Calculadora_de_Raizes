9a42359a2070fadb92a61f7dd5aae2d4d2613c5f
Giovanna Cavalcanti de Lima <95053005+GiovannaCavalcanti@users.noreply.github.com>
2022-03-30 15:21:32 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+const numero = null;
+
+function raiz(numero) {
+  if((numero ** 0.5) * 10 % 10 === 0 ){
+    console.log("A raiz desse numero é Perfeita");
+  } else {
+    console.log("A Raiz desse Numero é imperfeita");
+  }
+}
+
+raiz(13);//qualquer numero que queira testar ^-^//
